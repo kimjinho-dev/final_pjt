d83c2bb7adc19a1db4ebcf27291e697594649872
kimjinho-dev <kimjinho.dev@gmail.com>
2022-11-16 07:53:00 +0900

    back

---
+++ back/articles/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
+
+# Create your models here.
+class Article(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # tags = ArrayField(models.CharField(max_length=20), blank=True)
+
+# class Comment(models.Model):
+#     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+#     content = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
---
+++ back/server/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('articles.urls')),
+    path('accounts/', include('dj_rest_auth.urls')),
+    path('accounts/signup/', include('dj_rest_auth.registration.urls'))
+]
